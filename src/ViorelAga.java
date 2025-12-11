cdf2b4a737d03b733640df65d1da63a32386dd2b
Viorel Aga <viorel.aga@gmail.com>
2025-12-11 18:37:07 +0100

    Add text

---
+++ src/ViorelAga.java
@@ -0,0 +1,13 @@
+public class ViorelAga {
+    public static void main(String[] args) {
+
+        int varsta = 45;
+
+        if(varsta < 18){
+            System.out.println("Copil");
+
+        }else {
+            System.out.println("Adult");
+        }
+    }
+}
